f543489a0420d31593c38fb60fb59ff261cb3e85
Alberto <albertoulin619@gmail.com>
2023-09-08 01:59:41 -0600

    Actualización: se agrego el componente create-acount

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { CreateAcountComponent } from './create-acount/create-acount.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CreateAcountComponent
   ],
   imports: [
     BrowserModule,
