07123b4842f686de766b7ce5e55a79bed79a91bd
Alberto Ulin <albertoulin619@gmail.com>
2023-11-01 20:45:10 -0600

    01/11/2023

--- src/app/pages/select-type-user/select-type-user.component.ts
+++ src/app/pages/select-type-user/select-type-user.component.ts
@@ -14,5 +14,6 @@ export class SelectTypeUserComponent {
 
   enviarTipoUsuario(idTipoUsuario: TipoUsuario){
     this.onSelectTipoUsuario.emit(idTipoUsuario);
+    console.log(this.onSelectTipoUsuario.emit(idTipoUsuario))
   } 
 }
